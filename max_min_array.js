8b315cf2d806a1b9342a663a741c918d9dd1166c
sampoornaagrawal <44267132+sampoornaagrawal@users.noreply.github.com>
2019-11-25 18:50:36 +0530

    Update max_min_array.js

--- max_min_array.js
+++ max_min_array.js
@@ -1,3 +1,11 @@
 const number=[1,2,3,4,5,1];
 console.log(Math.min(...number));  // minimum
 console.log(Math.max(...number));  // maximum
+
+//  OR
+
+const final=number.reduce((a,b)=>{return a>b ? a : b })   // Maximum
+console.log(final);
+
+const final=number.reduce((a,b)=>{return a<b ? a : b })   // Minimum
+console.log(final);
